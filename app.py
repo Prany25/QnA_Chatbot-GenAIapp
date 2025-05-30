e6ddd85d583efaee45f4c22373e053b45e152aca
Prany25 <118735414+Prany25@users.noreply.github.com>
2025-05-31 00:02:20 +0530

    Final Q&A Chatbot with small model from Ollama

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.llms import Ollama
 
 import os
 from dotenv import load_dotenv
@@ -12,17 +13,32 @@ os.environ["LANGCHAIN_PROJECT"]="QnA Chatbot with Ollama"
 
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system":"You are a helpful assistant. Respond to the queries with what you know"),
-        ("user","Question:{question}")
+        ("system", "You are a helpful assistant. Respond to the queries with what you know"),
+        ("user", "Question:{question}")
     ]
 )
 
-def generate_response(question,api_key,llm,temperature,max_tokens):
-    openai.api_key=api_key
-    llm=ChatOpenAI(model=llm)
+def generate_response(question,engine,temperature,max_tokens):
+    llm=Ollama(model=engine)
     output_parser=StrOutputParser()
     chain= prompt|llm|output_parser
-    answer=chain.invoke({'question':question)
+    answer=chain.invoke({'question':question})
     return answer
 
+st.title("Q&A Chatbot with Ollama")
 
+st.sidebar.title("Settings")
+
+llm=st.sidebar.selectbox("Select an Ollama model",["Smollm2:135M"])
+
+temperature=st.sidebar.slider("Temperature",min_value=0.0,max_value=1.0,value=0.7)
+max_tokens=st.sidebar.slider("Max Tokens",min_value=50,max_value=300,value=150)
+
+st.write("Go ahead and ask any question!")
+user_input=st.text_input("You:")
+
+if user_input:
+    response=generate_response(user_input,llm,temperature,max_tokens)
+    st.write(response)
+else:
+    st.write("Please provide the query")
